3888de5103020723399a8f65dd5479027d5d199d
love olubummo <olubummolove3@gmail.com>
2024-01-12 09:55:12 +0100

    modified navbar

---
+++ app/assets/index.js
@@ -0,0 +1,27 @@
+import mcatLogin from "./mcat-logo.jpeg";
+import mcat2 from "./mcat2.jpeg";
+import mcat3 from "./mcat3.jpeg";
+import mcat4 from "./mcat4.jpeg";
+import mcat5 from "./mcat5.jpeg";
+import mcat6 from "./mcat6.jpeg";
+import mcat7 from "./mcat7.jpeg";
+import mcat8 from "./mcat8.jpeg";
+import qrCode from "./qr-code.jpeg";
+import telegram from "./telegram.png";
+import twitter from "./twitter.png";
+
+const images = {
+  mcatLogin,
+  mcat2,
+  mcat3,
+  mcat4,
+  mcat5,
+  mcat6,
+  mcat7,
+  mcat8,
+  qrCode,
+  telegram,
+  twitter,
+};
+
+export default images;
