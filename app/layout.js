748f35da34146e3460f11b5727a13f23f90c5a5b
love olubummo <olubummolove3@gmail.com>
2024-01-15 10:06:17 +0100

    test2

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Fraunces } from "next/font/google";
 import "./globals.scss";
 import { Footer, NavBar } from "./components";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const fraunces = Fraunces({
   subsets: ["latin"],
@@ -19,6 +21,7 @@ export default function RootLayout({ children }) {
         <NavBar />
         {children}
         <Footer />
+        <ToastContainer />
       </body>
     </html>
   );
